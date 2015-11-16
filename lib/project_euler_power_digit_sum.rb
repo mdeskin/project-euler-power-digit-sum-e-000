d6896c0a1c10da01f6d4e88e73ee61d21ca673a9
Mike Deskin <michaelhaydendeskin@gmail.com>
2015-11-16 01:16:07 +0000

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,11 @@
-# code your solution here
+def power_digit_sum(x,n)
+  count = 0
+  sum = 0
+  product = x ** n
+  product_str = product.to_s
+   until product_str.slice(count).nil?
+      sum = sum + product_str.slice(count).to_i
+      count += 1
+   end
+  sum
+end
